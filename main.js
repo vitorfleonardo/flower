a8688dfbe19fa25ce4891d8ce6f0d4a21ed493f6
vitorfleonardo <vitorfleonardo@gmail.com>
2024-09-28 01:14:34 -0300

    change for image

--- main.js
+++ main.js
@@ -4,3 +4,14 @@ onload = () => {
     clearTimeout(c);
   }, 1000);
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Seleciona a imagem que será exibida
+  const flowerImage = document.getElementById('flower-image');
+
+  // Função para exibir a imagem após 2 segundos
+  setTimeout(function () {
+    flowerImage.classList.remove('hidden-image'); // Remove a classe que oculta a imagem
+    flowerImage.classList.add('visible-image'); // Adiciona a classe para exibir a imagem
+  }, 2000); // 2000 milissegundos = 2 segundos
+});
